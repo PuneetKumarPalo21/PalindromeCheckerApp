3814265be7a5a9203db22fa1d5f095a07967c747
“PuneetKumarPalo21” <puneetkumarpalo@Puneets-Macbook.local>
2026-02-27 10:34:50 +0530

    UC2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,21 @@
 public class PalindromeCheckerApp {
+
+    /**
+     * Application entry point for UC2.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        String input = "madam";
+        boolean result = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                result = false;
+                break;
+            }
+        }
+
+        System.out.println("Is it a Palindrome?: " + result);
+    }
+
 }
